07506737ca214057245ad4d5927a6449dbd78805
Thilina Buddhika <thilinamb@gmail.com>
2018-09-10 01:14:30 -0600

    Daily work.

--- epi_judge_java/epi/IsTreeBalanced.java
+++ epi_judge_java/epi/IsTreeBalanced.java
@@ -1,20 +1,51 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class IsTreeBalanced {
 
-  @EpiTest(testDataFile = "is_tree_balanced.tsv")
+    @EpiTest(testDataFile = "is_tree_balanced.tsv")
 
-  public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
-  }
+    public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
+        if(tree == null){
+            return true;
+        }
+        AtomicBoolean status = new AtomicBoolean(true);
+        calculateHeight(tree, status);
+        return status.get();
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsTreeBalanced.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static int calculateHeight(BinaryTreeNode<Integer> root, AtomicBoolean isBalanced) {
+        if (root.left == null && root.right == null) {
+            return 0;
+        }
+        int leftHeight = 0;
+        int rightHeight = 0;
+        AtomicBoolean rightStatus = new AtomicBoolean(true);
+        AtomicBoolean leftStatus = new AtomicBoolean(true);
+        if (root.left != null) {
+            leftHeight = calculateHeight(root.left, leftStatus) + 1;
+        }
+        if (root.right != null) {
+            rightHeight = calculateHeight(root.right, rightStatus) + 1;
+        }
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            isBalanced.set(false);
+        } else {
+            isBalanced.set(isBalanced.get() & leftStatus.get() & rightStatus.get());
+        }
+        return Math.max(leftHeight, rightHeight);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsTreeBalanced.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
--- epi_judge_java/epi/SortedListsMerge.java
+++ epi_judge_java/epi/SortedListsMerge.java
@@ -1,20 +1,50 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class SortedListsMerge {
-  @EpiTest(testDataFile = "sorted_lists_merge.tsv")
-  //@include
-  public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
-                                                      ListNode<Integer> L2) {
-    // TODO - you fill in here.
-    return null;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SortedListsMerge.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class SortedListsMerge {
+    @EpiTest(testDataFile = "sorted_lists_merge.tsv")
+    //@include
+    public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
+                                                        ListNode<Integer> L2) {
+        ListNode<Integer> head = null;
+        ListNode<Integer> currentNode = null;
+        while (L1 != null || L2 != null) {
+            ListNode<Integer> nextNode;
+            if (L1 == null) {
+                nextNode = L2;
+                L2 = L2.next;
+            } else if (L2 == null) {
+                nextNode = L1;
+                L1 = L1.next;
+            } else {
+                if (L1.data < L2.data) {
+                    nextNode = L1;
+                    L1 = L1.next;
+                } else {
+                    nextNode = L2;
+                    L2 = L2.next;
+                }
+            }
+
+            if (head == null) {
+                currentNode = nextNode;
+                head = nextNode;
+            } else {
+                currentNode.next = nextNode;
+                currentNode = currentNode.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SortedListsMerge.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
